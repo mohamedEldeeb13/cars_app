e1edcac15ee75d6705ab53f317284eb9f620d50a
mohamedEldeeb13 <mooel744@gmail.com>
2023-12-30 19:23:15 +0200

    Initial Commit

---
+++ cars_app/cars_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  cars_appApp.swift
+//  cars_app
+//
+//  Created by Mohamed Abd Elhakam on 30/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct cars_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
